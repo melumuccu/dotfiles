c5cada850667636c9ebbd277abf5721a588e4303
Koki Fujisawa <melumuccu413@gmail.com>
2026-04-08 17:07:07 +0900

    Remove .config/fish/conf.d/29-abbr-media.fish

--- dot_config/private_fish/conf.d/29-abbr-media.fish
+++
@@ -1,6 +0,0 @@
-if status is-interactive
-    abbr --add movtogif 'my_mov_to_gif'
-    abbr --add cw 'my_convert_webp'
-    abbr --add ch 'my_convert_heic_to_jpeg'
-    abbr --add wh 'my_width_height_convert'
-end
