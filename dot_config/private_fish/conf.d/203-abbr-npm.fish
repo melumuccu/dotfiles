3e62e32b3e7bd2e1625dfea67a75ec7a952ad25c
Koki Fujisawa <melumuccu413@gmail.com>
2026-04-13 23:42:53 +0900

    Update .config/fish/conf.d/203-abbr-npm.fish Update Library/Application Support/Code/User/settings.json

--- dot_config/private_fish/conf.d/203-abbr-npm.fish
+++ dot_config/private_fish/conf.d/203-abbr-npm.fish
@@ -2,5 +2,6 @@ if status is-interactive
     abbr --add ni 'pnpm install'
     abbr --add nui 'pnpm uninstall'
     abbr --add nr 'pnpm run'
+    abbr --add npx 'pnpm dlx'
     abbr --add fnr 'my_npm_run_script'
 end
