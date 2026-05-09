345087a5db91aa7f410658bec8c94a0818f15ba4
Koki Fujisawa <melumuccu413@gmail.com>
2026-05-09 18:26:12 +0900

    Update .agents/.skill-lock.json Update .config/fish/conf.d/202-abbr-mise.fish Update .config/fish/fish_variables Update Library/Application Support/Code/User/settings.json

--- dot_config/private_fish/conf.d/202-abbr-mise.fish
+++ dot_config/private_fish/conf.d/202-abbr-mise.fish
@@ -1,4 +1,6 @@
 if status is-interactive
-    abbr --add mr 'mise run'
+    abbr --erase mr 2>/dev/null
+    abbr --erase mise 2>/dev/null
+    abbr --add mr 'my_mise_run'
     abbr --add mise 'mise run start'
 end
